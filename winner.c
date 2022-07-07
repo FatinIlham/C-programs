c0ff4396004bbfbc7b07718e4e8de4a66bb2c7d1
Fatin Ilham <mdfatin.ilham@gmail.com>
2022-07-07 09:43:38 +0600

    Indentation changes made

--- winner.c
+++ winner.c
@@ -1,52 +1,30 @@
 #include<stdio.h>
 #include<iostream>
 #include<stdlib.h>
-
 using namespace std;
-
 struct Winner{
-
     int code;
-
-void display()
-{
-    cout<<"the winner is  :"<<this->code<<endl;
-
-}
-
-
+    void display(){
+        cout<<"the winner is: "<<this->code<<endl;
+    }
 };
-
-
-void displayall(struct Winner c[], int sze)
-{
-  for(int i =0;i<sze;i++)
-  {
-
+void displayall(struct Winner c[], int sze){
+  for(int i =0;i<sze;i++){
     c[i].display();
-
   }
-
 }
 int main()
 {
     struct Winner c[5];
-
     struct Winner ilham;
-   ilham.code = 101102;
-
+    ilham.code = 101102;
     struct Winner reza;
     reza.code= 101120;
-
     struct Winner shojib;
     shojib.code =101102;
-
     struct Winner oronno;
-
     oronno.code =100012;
-
     struct Winner shuvo;
-
     shuvo.code = 102342;
 
     c[0] = ilham;
